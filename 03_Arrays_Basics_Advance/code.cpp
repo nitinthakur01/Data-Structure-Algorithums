62fa5cbc0ced9032a5183eb530e13aada75b436e
Nitin Thakur <nitinthakur46323@gmail.com>
2026-07-17 16:25:21 +0530

    array advance

--- 03_Arrays_Basics_Advance/code.cpp
+++ 03_Arrays_Basics_Advance/code.cpp
@@ -6,6 +6,8 @@ using namespace std;
 int main() {
     // ARRAYS (Basic to Advance) :-
 
+    // BASIC QUESTIONS
+
     // 1. Sum of arrays element
 
     // int nums[] = {5, 6, 7, 8, 4};
@@ -46,7 +48,7 @@ int main() {
     // 3. Second max element from an array
 
     // int nums[] = {5, 15, 22, 1, -15, 24, 24};
-    // int size = 6;
+    // int size = 7;
 
     // int maxVal = INT_MIN;
     // int secondMax = INT_MIN;
@@ -64,17 +66,18 @@ int main() {
     // cout << maxVal << endl; // 24
     // cout << secondMax << endl; // 22
 
-    // 4. Reverse the array
+    // 4. Reverse the array (Imp)
 
     // int arr[] = {4, 2, 7, 8, 1, 2, 5};
     // int size = 7;
     // int start = 0, end = size - 1; // two pointers method
 
-    // while(start < end){ // Once start and end meet (or cross each other)
+    // while(start < end){ // Once start and end meet 
     //     // swaping values
-    //     int temp = arr[start]; 
+    //     int temp = arr[start]; // we can also use built in swap function
     //     arr[start] = arr[end];
     //     arr[end] = temp;
+    //     // increment from start and decrement from end of an array
     //     start++;
     //     end--;
     // }
@@ -84,29 +87,72 @@ int main() {
     // }
     // cout << endl;
 
-    // 5. All zeroes to left and all ones to right
+    // 5. All zeroes to left and all ones to right (V.Imp)
 
     // int arr[] = {1, 0, 1, 0, 0, 1, 1, 0};
     // int size = 8;
     // int start = 0, end = size - 1; // two pointers method
 
-    //  while (start < end) { // Once start and end meet (or cross each other)
+    //  while (start < end) { // Once start and end meet 
     //     if (arr[start] == 0) {
-    //         start++;
+    //         start++; // All zeroes should be in left (move right to left)
     //     }
     //     else if (arr[end] == 1) {
-    //         end--;
+    //         end--; // All ones should be in right (move left to right)
     //     }
     //     else {
-    //         swap(arr[start], arr[end]);
+    //         int temp = arr[start]; // we can also use built in swap function
+    //         arr[start] = arr[end];
+    //         arr[end] = temp;
     //         start++;
     //         end--;
     //     }
     // }
-    // // To show new array
+    // To show our updated array
     // for (int i = 0; i < size; i++) {
     //     cout << arr[i] << " "; // 0 0 0 0 1 1 1 1 
     // }
+    // cout << endl;
 
+    // ADVANCE QUESTIONS
+
+    // 1. Maximum Subarray Sum / Kadane's Algorithum (V.Imp)
+
+    // int arr[] = {3, -4, 5, 4, -1, 7, -8};
+    // int size = 7;
+    // int currSum = 0,  maxSum = INT_MIN;
+
+    // // time complexity is O(n) (linear)
+    // for(int i = 0; i < size; i++) {
+    //     currSum = currSum + arr[i];
+    //     // Calculate maxSum Value
+    //     maxSum = max(currSum, maxSum); 
+    //     // Reset currSum to 0, if currSum value is -ve
+    //     if(currSum < 0){
+    //         currSum = 0;
+    //     }
+    // }
+    // cout << maxSum << endl; // 15 (maxSubarray is {5, 4, -1, 7})
+
+    // 2. Majority Element / Moore's Voting Algorithum (V.Imp)
+
+    // int arr[] = {1, 2, 2, 1, 1, 1, 2};
+    // int size = 7;
+    // int freq = 0, ans = 0;
+
+    // // time complexity is O(n) (linear)
+    // for(int i = 0; i < size; i++){
+    //     if(freq == 0){ 
+    //         ans = arr[i];
+    //     }
+    //     if(ans == arr[i]){ 
+    //         freq ++; // increament if ans is equal to arr[i]
+    //     }
+    //     else{
+    //         freq --; // decreament if ans is not equal to arr[i]
+    //     }
+    // }
+    // cout << ans << endl; // 1
+    
     return 0;
 }
