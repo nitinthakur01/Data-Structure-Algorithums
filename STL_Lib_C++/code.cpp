4422f093d273a2c253f198f245dedffe733752bc
Nitin Thakur <nitinthakur46323@gmail.com>
2026-09-17 18:59:35 +0530

    STL

--- STL_Lib_C++/code.cpp
+++ STL_Lib_C++/code.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
 #include <vector>
+#include <list>
+#include <deque>
+#include <stack>
+#include <queue>
 using namespace std;
 // To run : g++ code.cpp -o code; .\code.exe
 
 int main() {
     // STL : (standard template library)
 
-    // CONTAINERS :-
+    // CONTAINERS :- (Sequential)
+    /*
     // 1. vectors
     vector<int> vec;
 
@@ -58,6 +63,71 @@ int main() {
     vector<int> vecIt = {1, 2, 3, 4, 5};
     cout << "begin : " << *(vecIt.begin()) << endl; // 1
     cout << "end : " << *(vecIt.end()) << endl; // 0
+
+    // 2. lists
+    list <int> l;
+    l.push_back(1);
+    l.push_back(2);
+    l.push_front(3);
+    l.push_front(4);
+
+    for(int val : l){
+        cout << val << " "; // 4 3 1 2 
+    }
+    cout << endl;
+
+    // 3. deques
+    deque <int> d;
+    d.push_back(1);
+    d.push_back(2);
+    d.push_front(3);
+    d.push_front(4);
+
+    for(int val : d){
+        cout << val << " "; // 4 3 1 2 
+    }
+    cout << endl;
+    */
+    // CONTAINERS :- (Non-Sequential)
+   // 1. stack
+    stack <int> s;
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    s.push(4);
+
+    while(!s.empty()){
+        cout << s.top() << " "; // 4 3 2 1
+        s.pop();
+    }
+    cout << endl;
+
+    // 2. queue
+    queue <int> q;
+    q.push(1);
+    q.push(2);
+    q.push(3);
+    q.push(4);
+
+    while(!q.empty()){
+        cout << q.front() << " "; // 1 2 3 4
+        q.pop();
+    }
+    cout << endl;
+
+    // priority queue
+    priority_queue <int> pq;
+    pq.push(5);
+    pq.push(3);
+    pq.push(10);
+    pq.push(4);
+
+    while(!pq.empty()){
+        cout <<pq.top() << " "; // 10 5 4 3
+       pq.pop();
+    }
+    cout << endl;
+
 
     return 0;
 
